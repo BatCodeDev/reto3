a7a21fbe54e0d3ee2e2af9056d0f11d036d47436
Alejandro <alexddo122@gmail.com>
2019-02-06 12:53:18 +0100

    Favicon + fix limit count products

--- model/Order.php
+++ model/Order.php
@@ -89,7 +89,7 @@ class Order extends Generic{
     }
     public function getProductCount(){
         $res = parent::getConnection()->prepare(
-            "select o.idProduct as idProduct,count(o.idOrder) as pQuantity, p.name as name FROM orderproduct o, product p WHERE o.idProduct = p.ID GROUP BY idProduct ORDER BY idProduct DESC"
+            "select o.idProduct as idProduct,count(o.idOrder) as pQuantity, p.name as name FROM orderproduct o, product p WHERE o.idProduct = p.ID GROUP BY idProduct ORDER BY idProduct DESC LIMIT 5"
         );
         $res->execute();
         return $res->fetchAll();
